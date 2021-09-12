4e47d46986fb2dad29879a9dcfe08f59bb0b234c
Aleksandra Zhzhonova <a.zhzhonova@corp.mail.ru>
2021-09-13 02:18:49 +0300

    pick product item test

--- testrunner-toolkit/testcafe/tests/specs/login.spec.js
+++ testrunner-toolkit/testcafe/tests/specs/login.spec.js
@@ -1,13 +1,9 @@
-import { LOGIN_USERS } from '../configs/e2eConstants';
-
-import { Selector, Browser,ClientFunction } from 'testcafe';
+import { Selector, Browser, ClientFunction } from 'testcafe';
 
 
 fixture`Test Input`
   .page(`https://www.wildberries.ru/`)
-  .beforeEach(async t => {
-    await t.resizeWindow(1400, 900);
-  });
+;
 
 test('should be able to test loading of login page', async t => {
 
@@ -25,21 +21,14 @@ const divSelector = Selector('#catalog > div:nth-child(1) > div.catalog-page > d
   await t.expect(getLocation()).contains('FABRETTI')
   await t.wait(10000).expect(divSelector.exists).ok({timeout:5000});
 
+  //Сашин говнокод
+  const listItem = Selector('.product-card');
+  const res = Selector('.searching-results');
+  const bottomItemIndex = await listItem.count -1;
+  const bottomItem = listItem.nth(bottomItemIndex);
+  await t.click(res);
+  await t.hover(bottomItem);
+  const i = Math.floor(Math.random() * bottomItemIndex)
+  await t.click(listItem.nth(i));
+  await t.wait(5000);
 });
-
-// test('should be able to test loading of login page', async t => {
-//   await t.expect(LoginPage.isScreenDisplayed()).ok();
-// });
-
-// test('should be able to login with a standard user', async t => {
-//   await LoginPage.signIn(LOGIN_USERS.STANDARD);
-//   await t.expect(SwagListPage.screen.exists).ok();
-// });
-
-// test('should not be able to login with a locked user', async t => {
-//   // It doesn't matter which error we check, all errors should be checked in a UT
-//   // With this UT we just check that A failure is triggered
-//   await t.expect(LoginPage.isErrorMessageDisplayed()).notOk();
-//   await LoginPage.signIn(LOGIN_USERS.LOCKED);
-//   await t.expect(LoginPage.getErrorMessage()).eql('Epic sadface: Sorry, this user has been locked out.');
-// });
