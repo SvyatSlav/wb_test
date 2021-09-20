aa2c2e654690525d66e3053f8976f4efa74f5621
Sven <sanslav@gmail.com>
2021-09-20 22:42:03 +0300

    test

--- tests/myip.test.js
+++ tests/myip.test.js
@@ -1,7 +1,15 @@
 import { Selector, Browser, ClientFunction } from 'testcafe';
 
-fixture`MyIP`
+fixture`MyIP UA`
   .page(`https://www.whatismyip.com/user-agent/`);
+
+test('get myip page', async t => {
+  await t.wait(5000);
+});
+
+
+fixture`MyIP IP`
+  .page(`https://www.whatismyip.com/my-ip-information/`);
 
 test('get myip page', async t => {
   await t.wait(5000);
